18659adb8fe8813f02387570ee753a44a574c958
unknown <bogdanlikuliks@hotmail.com>
2020-10-17 07:21:16 +0200

    Infinite Survival - drawing the projectiles - ver 1.02

--- Infinite Survival/app.js
+++ Infinite Survival/app.js
@@ -5,6 +5,13 @@ canvas.width = innerWidth
 canvas.height = innerHeight
 
 
+let projectiles = [];
+
+addEventListener('click', () => {
+
+  const projectile = new Projectile(canvas.width / 2, canvas.height / 2, 10, 'green', {x:1, y:1})
+  projectiles.push(projectile)
+})
 
 // Player
 class Player {
@@ -28,14 +35,42 @@ class Player {
   }
 }
 
+// Projectile
+class Projectile {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+    this.velocity = velocity
+  }
+
+  draw() {
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    ctx.fillStyle = this.color
+    ctx.fill()
+    ctx.closePath()
+  }
+
+  update() {
+    this.x = this.x + this.velocity.x
+    this.y = this.y + this.velocity.y
+    this.draw()
+  }
+}
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
-let player = new Player(x, y, 20, 'red')
+let player = new Player(x, y, 20, 'purple')
 
 function animate() {
   requestAnimationFrame(animate)
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   player.update()
+  projectiles.forEach(projectile => {
+    projectile.update();
+  });
 }
 
 animate()
