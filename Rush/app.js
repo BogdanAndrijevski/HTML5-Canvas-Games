3d0f8a5f0bf954605f5dd6bbd2d292bdcf015890
unknown <bogdanlikuliks@hotmail.com>
2020-11-09 17:49:19 +0100

    Rush - adding the traffic car - ver 1.02

--- Rush/app.js
+++ Rush/app.js
@@ -17,6 +17,9 @@ canvas.style.border = "2px solid red";
 let playerCarImg = new Image();
 playerCarImg.src = 'img/PlayerCar.png';
 
+let trafficCarImg = new Image();
+trafficCarImg.src = 'img/TrafficCar.png';
+
 let car = {
   width: 45,
   height: 70,
@@ -29,6 +32,12 @@ let car = {
     down : false,
   }
 }
+
+let velocityForLinesAndCars = 0.001;
+
+let trafficCars = [];
+let playerCar;
+
 
 //===========================================
 // keyboard movement
@@ -101,16 +110,79 @@ class PlayerCar {
   }
 }
 
+class TrafficCar {
+  constructor(image, x, y, width, height) {
+      this.image = image
+      this.x = x;
+      this.y = y;
+      this.width = width;
+      this.height = height;
+      this.velocity = {
+          y: 2
+      }
+  }
+
+  draw() {
+      ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+  }
+  update() {
+      this.velocity.y += velocityForLinesAndCars;
+      this.y += this.velocity.y;
+
+
+      // so othey wont spawn on top of eachother
+      if (this.y > canvas.height) {
+          let x = (Math.random() * (canvas.width - car.width));
+          let y = (Math.random() * 600) - 700;
+          for (let i = 0; i < trafficCars.length; i++) {
+              const trafficCar = trafficCars[i];
+              if (x < trafficCar.x + trafficCar.width && x + car.width > trafficCar.x
+                  && y < trafficCar.y + trafficCar.height && y + car.height > trafficCar.y) {
+
+                  x = (Math.random() * (canvas.width - car.width));
+                  y = (Math.random() * 600) - 700;
+                  i = -1;
+              }
+          }
+          this.x = x;
+          this.y = y;
+      }
+      this.draw()
+  }
+}
+
 init();
 animate();
 
 function init() {
   playerCar = new PlayerCar(playerCarImg, canvas.width / 2 - car.width / 2, canvas.height - car.height, car.width, car.height)
+
+ // Traffic Cars
+ for (let i = 0; i < 10; i++) {
+  let x = (Math.random() * (canvas.width - car.width));
+  let y = (Math.random() * (600)) - 700;
+
+  for (let j = 0; j < trafficCars.length; j++) {
+      if (x < trafficCars[j].x + trafficCars[j].width && x + car.width > trafficCars[j].x
+          && y < trafficCars[j].y + trafficCars[j].height && y + car.height > trafficCars[j].y) {
+          x = (Math.random() * (canvas.width - car.width));
+          y = (Math.random() * (600)) - 700;
+          j = -1;
+      }
+
+  }
+  trafficCars.push(new TrafficCar(trafficCarImg, x, y, car.width, car.height))
+
+}
 }
 
 function animate() {
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  trafficCars.forEach(trafficCar => {
+    trafficCar.update();
+});
 
   playerCar.update();
 
