699773d34fdbdf27c6a76c49149b6c7b2c2ccbb2
unknown <bogdanlikuliks@hotmail.com>
2021-01-17 15:09:58 +0100

    Tetris - adding rotation - ver 1.04

--- Tetris/app.js
+++ Tetris/app.js
@@ -72,7 +72,39 @@ function collide(arena, player) {
   return false;
 }
 
+function playerRotate(dir) {
+  const pos = player.pos.x;
+  let offset = 1;
+  rotate(player.matrix, dir);
+  while (collide(arena, player)) {
+    player.pos.x += offset;
+    offset = -(offset + (offset > 0 ? 1 : -1));
+    if (offset > player.matrix[0].length) {
+      rotate(player.matrix, -dir);
+      player.pos.x = pos;
+      return;
+    }
+  }
+}
 
+function rotate(matrix, dir) {
+  for (let y = 0; y < matrix.length; y++) {
+    for (let x = 0; x < y; x++) {
+      [
+        matrix[x][y],
+        matrix[y][x],
+      ] = [
+          matrix[y][x],
+          matrix[x][y],
+        ]
+    }
+  }
+  if (dir > 0) {
+    matrix.forEach(row => row.reverse())
+  } else {
+    matrix.reverse()
+  }
+}
 function playerDrop() {
   player.pos.y++;
   if (collide(arena, player)) {
@@ -114,7 +146,7 @@ animate();
 
 function playerMove(dir) {
   player.pos.x += dir;
-  if(collide(arena, player)){
+  if (collide(arena, player)) {
     player.pos.x -= dir;
   }
 
@@ -126,5 +158,9 @@ document.addEventListener('keydown', e => {
     playerMove(1);
   } else if (e.key === 's') {
     playerDrop()
+  } else if (e.key === 'q') {
+    playerRotate(-1);
+  } else if (e.key === 'w') {
+    playerRotate(1);
   }
 });
