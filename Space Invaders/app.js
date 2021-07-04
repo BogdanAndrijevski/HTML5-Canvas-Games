415bb049c780db85b07378e5f099552d2df0927a
unknown <bogdanlikuliks@hotmail.com>
2021-07-04 02:09:06 +0200

    Space Invaders - first commit - ver 1.00

---
+++ Space Invaders/app.js
@@ -0,0 +1,14 @@
+let canvas = document.querySelector('canvas');
+canvas.width = 500;
+canvas.height = 550;
+let ctx = canvas.getContext('2d');
+
+// Center Canvas with JavaScriptws
+canvas.style.display = "block";
+canvas.style.position = "absolute";
+canvas.style.margin = "auto";
+canvas.style.top = 0;
+canvas.style.bottom = 0;
+canvas.style.left = 0;
+canvas.style.right = 0;
+canvas.style.border = "2px solid red"
